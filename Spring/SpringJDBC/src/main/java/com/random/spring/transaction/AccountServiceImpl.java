dec734063759672942cfcd7cf0059e476325537c
randomj182 <983851677@qq.com>
2018-06-13 22:53:07 +0800

    手动编码的方式完成事务管理

--- Spring/SpringJDBC/src/main/java/com/random/spring/transaction/AccountServiceImpl.java
+++ Spring/SpringJDBC/src/main/java/com/random/spring/transaction/AccountServiceImpl.java
@@ -1,18 +1,32 @@
 package com.random.spring.transaction;
 
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
+
 public class AccountServiceImpl implements AccountService{
 	
 	private AccountDAO accountDAO;
+	private TransactionTemplate transactionTemplate;
 	
 	public void setAccountDAO(AccountDAO accountDAO) {
 		this.accountDAO = accountDAO;
 	}
 	
+	public void setTransactionTemplate(TransactionTemplate transactionTemplate) {
+		this.transactionTemplate = transactionTemplate;
+	}
+	/*第四步:在业务层代码上使用模板:*/
 	@Override
 	public void transfer(String from, String to, Double money) {
-		accountDAO.out(from, money);
-		int d = 1/0;
-		accountDAO.in(to, money);
+		transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+			@Override
+			protected void doInTransactionWithoutResult(TransactionStatus arg0) {
+				accountDAO.out(from, money);
+				int d = 1/0;
+				accountDAO.in(to, money);
+			}
+		});
 	}
 
 }
