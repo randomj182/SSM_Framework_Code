a8fb977445c2a0bdb9e201e1970413c33eb9e607
randomj182 <983851677@qq.com>
2018-08-02 11:43:30 +0800

    file读取文件的路径问题

--- Mybatis/Mybatis_Generator/src/main/java/GeneratorSqlmap.java
+++ Mybatis/Mybatis_Generator/src/main/java/GeneratorSqlmap.java
@@ -18,9 +18,16 @@ public class GeneratorSqlmap {
 		List<String> warnings = new ArrayList<String>();
 		boolean overwrite = true;
 		//指定 逆向工程配置文件
-		//File configFile = new File("generatorConfig.xml"); 
+		// 全路径 即绝对路径   1.带盘符,如D:/			2.以http开头	http://
+		//File configFile = new File("D:/eclipse/eclipse_git_repo/SSM_Framework_Code/Mybatis/Mybatis_Generator/src/main/resources/generatorConfig.xml"); 
+		
+		// 相对路径:不带/ --->相对的是以项目名为根路径,所以在全路径的基础上去掉项目名
+        //File configFile = new File("src/main/resources/generatorConfig.xml");
+        
+		// 相对路径:不带/ 
 		String genCfg = "generatorConfig.xml";
         File configFile = new File(GeneratorSqlmap.class.getResource(genCfg).getFile());
+		
 		ConfigurationParser cp = new ConfigurationParser(warnings);
 		Configuration config = cp.parseConfiguration(configFile);
 		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
